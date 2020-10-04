b0d6fea56f9b21a8eabb810a2feb0aa13c4c69f7
yakimovab <A.Yakimov@softline.com>
2020-10-04 20:30:56 +0500

    add new instance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-
+from pyrogram.types import Message
 from pyrogram.types import ChatPermissions
 
 import time
@@ -119,5 +119,125 @@ def thanos(_, msg) :
 			time.sleep(e.x)
 
 	app.send_message(chat, "Но какой ценой?")
+
+
+#autosroll
+f = filters.chat([])
+
+
+@app.on_message(f)
+def auto_read(_, message: Message):
+	app.read_history(message.chat.id)
+	message.continue_propagation()
+
+
+@app.on_message(filters.command("autoscroll", ".") & filters.me)
+def add_keep(_, message: Message):
+	if message.chat.id in f:
+		f.remove(message.chat.id)
+		message.edit("Autoscroll deactivated")
+	else:
+		f.add(message.chat.id)
+		message.edit("Autoscroll activated")
+
+#flip-text
+
+REPLACEMENT_MAP = {
+    "a": "ɐ",
+    "b": "q",
+    "c": "ɔ",
+    "d": "p",
+    "e": "ǝ",
+    "f": "ɟ",
+    "g": "ƃ",
+    "h": "ɥ",
+    "i": "ᴉ",
+    "j": "ɾ",
+    "k": "ʞ",
+    "l": "l",
+    "m": "ɯ",
+    "n": "u",
+    "o": "o",
+    "p": "d",
+    "q": "b",
+    "r": "ɹ",
+    "s": "s",
+    "t": "ʇ",
+    "u": "n",
+    "v": "ʌ",
+    "w": "ʍ",
+    "x": "x",
+    "y": "ʎ",
+    "z": "z",
+    "A": "∀",
+    "B": "B",
+    "C": "Ɔ",
+    "D": "D",
+    "E": "Ǝ",
+    "F": "Ⅎ",
+    "G": "פ",
+    "H": "H",
+    "I": "I",
+    "J": "ſ",
+    "K": "K",
+    "L": "˥",
+    "M": "W",
+    "N": "N",
+    "O": "O",
+    "P": "Ԁ",
+    "Q": "Q",
+    "R": "R",
+    "S": "S",
+    "T": "┴",
+    "U": "∩",
+    "V": "Λ",
+    "W": "M",
+    "X": "X",
+    "Y": "⅄",
+    "Z": "Z",
+    "0": "0",
+    "1": "Ɩ",
+    "2": "ᄅ",
+    "3": "Ɛ",
+    "4": "ㄣ",
+    "5": "ϛ",
+    "6": "9",
+    "7": "ㄥ",
+    "8": "8",
+    "9": "6",
+    ",": "'",
+    ".": "˙",
+    "?": "¿",
+    "!": "¡",
+    '"': ",,",
+    "'": ",",
+    "(": ")",
+    ")": "(",
+    "[": "]",
+    "]": "[",
+    "{": "}",
+    "}": "{",
+    "<": ">",
+    ">": "<",
+    "&": "⅋",
+    "_": "‾",
+}
+
+
+@app.on_message(filters.command("flip", prefixes=".") & filters.me)
+def flip(_, msg):
+    text = msg.text.split(".flip", maxsplit=1)[1]
+    final_str = ""
+    for char in text:
+        if char in REPLACEMENT_MAP.keys():
+            new_char = REPLACEMENT_MAP[char]
+        else:
+            new_char = char
+        final_str += new_char
+    if text != final_str:
+        msg.edit(final_str)
+    else:
+        msg.edit(text)
+
 
 app.run()
