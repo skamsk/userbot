62275c43db3a14f7e6add7383415c9cf6ca938a0
skamsk <skamsk@list.ru>
2021-10-07 18:16:28 +0500

    Update commands.py

--- handlers/commands.py
+++ handlers/commands.py
@@ -10,7 +10,7 @@ from pyrogram.types import ChatPermissions
 
 @Client.on_message(filters.command("start"))
 def start(bot: Client, message: Message):
-    #await message.reply_text(f"Hello MOTHER FUCKERS {message.from_user.mention}")
+    #await message.reply_text(f"Hello {message.from_user.mention}")
     tg_id = message.from_user.id
     print(message.text)
     msg = "HI HI HI "
